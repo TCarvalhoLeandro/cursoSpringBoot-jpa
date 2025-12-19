d088655b5e8b8f24161c09018f111454abc5721c
Leandro <trairicarvalho@gmail.com>
2025-12-19 10:45:48 -0300

    Projeto criado

---
+++ src/test/java/com/leandro/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
